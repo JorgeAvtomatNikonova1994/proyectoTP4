6269be31f10c7ef495386bc13d386dfae0832ef8
JorgeAvtomatNikonova1994 <41787273+JorgeAvtomatNikonova1994@users.noreply.github.com>
2018-09-10 22:46:15 -0300

    Update Aula_comun.java
    Agregado de Clave primaria de la clase y getters y setters

--- src/org/aula_comun/Aula_comun.java
+++ src/org/aula_comun/Aula_comun.java
@@ -1,18 +1,31 @@
 package org.aula_comun;
 
+import javax.persistence.*;
+
 import org.espacio_utilizado.*;
+import org.openxava.annotations.*;
 
 public class Aula_comun extends Espacio_utilizado {
+	@Id @Column(length=5) @Required
+	private int cod_aula_comun;
+	@Column(length=35) @Required
 	private String descripcion;
 	
-	
-/*GETTERS AND SETTERS*/
+	public int getCod_aula_comun() {
+		return cod_aula_comun;
+	}
+	public void setCod_aula_comun(int cod_aula_comun) {
+		this.cod_aula_comun = cod_aula_comun;
+	}
 	public String getDescripcion() {
 		return descripcion;
 	}
-
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
+	
+	
+/*GETTERS AND SETTERS*/
+
 	
 }
