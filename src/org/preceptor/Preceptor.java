2f9bb9ee9dfb9af40eef9c1c7d8fb9b58fe3b522
JorgeAvtomatNikonova1994 <41787273+JorgeAvtomatNikonova1994@users.noreply.github.com>
2018-09-10 22:56:20 -0300

    Update Preceptor.java
    Clase "finalizada"

--- src/org/preceptor/Preceptor.java
+++ src/org/preceptor/Preceptor.java
@@ -1,28 +1,35 @@
 package org.preceptor;
 
 import javax.persistence.*;
-
-import org.centroprofesores.*;
+import org.persona.*;
 import org.regencia.*;
 
 @Entity
-public class Preceptor extends Centrodeprofesores {
-	@Id
+public class Preceptor extends Persona {
+	@Id @Column(length=5)
 	private int cod_preceptor;
+	@Column(length=30)
 	private Regencia zona_regencia;
 	
-	/*GETTERS AND SETTERS*/
-public int getCod_preceptor() {
+	
+	public int getCod_preceptor() {
 		return cod_preceptor;
 	}
-public void setCod_preceptor(int cod_preceptor) {
+	public void setCod_preceptor(int cod_preceptor) {
 		this.cod_preceptor = cod_preceptor;
 	}
-public Regencia getZona_regencia() {
+	public Regencia getZona_regencia() {
 		return zona_regencia;
 	}
-public void setZona_regencia(Regencia zona_regencia) {
+	public void setZona_regencia(Regencia zona_regencia) {
 		this.zona_regencia = zona_regencia;
+	}
+	
+	
+	
+
+	/*GETTERS AND SETTERS*/
+
 	}
 
 }
