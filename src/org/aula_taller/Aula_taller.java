a55303fdfdb5fb00049277769974683d82accf68
JorgeAvtomatNikonova1994 <41787273+JorgeAvtomatNikonova1994@users.noreply.github.com>
2018-09-10 22:49:49 -0300

    Update Aula_taller.java
    Clase "terminada"

--- src/org/aula_taller/Aula_taller.java
+++ src/org/aula_taller/Aula_taller.java
@@ -1,5 +1,41 @@
-package org.aula_taller;
+package org.alumno;
 
-public class Aula_taller {
+import javax.persistence.*;
 
+import org.openxava.annotations.*;
+import org.persona.*;
+
+
+@Entity
+public class Alumno extends Persona{
+	@Id @Column(length=5) @Required
+	private int cod_alumno;
+	@Column(length=30) @Required
+	private String nombre;
+	@Column(length=30) @Required
+	private String apellido;
+	
+	
+	public int getCod_alumno() {
+		return cod_alumno;
+	}
+	public void setCod_alumno(int cod_alumno) {
+		this.cod_alumno = cod_alumno;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	/*GETTERS AND SETTERS*/
+
+	
 }
