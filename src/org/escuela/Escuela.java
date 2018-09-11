0a679da66d73e608489288818a5691570729771d
JorgeAvtomatNikonova1994 <41787273+JorgeAvtomatNikonova1994@users.noreply.github.com>
2018-09-10 22:50:32 -0300

    Update Escuela.java
    Clase "finalizada"

--- src/org/escuela/Escuela.java
+++ src/org/escuela/Escuela.java
@@ -1,5 +1,36 @@
 package org.escuela;
 
-public class Escuela {
+import javax.persistence.*;
 
+import org.openxava.annotations.*;
+import org.persona.*;
+import org.regencia.*;
+
+@Entity
+public class Escuela {
+	@Id @Column(length=30) @Required
+	private String nombre;
+	@Column(length=20) @Required
+	private String categoria;
+	
+	
+	@OneToOne
+	private Regencia regencia;
+	@ManyToOne
+	private Persona persona;
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
 }
