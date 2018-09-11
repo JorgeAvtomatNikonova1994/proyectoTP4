7eb4cf36787af797d254d8a0f0a5955e22ce34a8
JorgeAvtomatNikonova1994 <41787273+JorgeAvtomatNikonova1994@users.noreply.github.com>
2018-09-10 22:53:06 -0300

    Update Gimnasio.java
    Clase "finalizada"

--- src/org/gimnasio/Gimnasio.java
+++ src/org/gimnasio/Gimnasio.java
@@ -1,9 +1,18 @@
 package org.gimnasio;
 
-import org.espacio_utilizado.*;
+import javax.persistence.*;
 
+import org.espacio_utilizado.*;
+import org.openxava.annotations.*;
+
+@Entity
 public class Gimnasio extends Espacio_utilizado {
+	@Id @Required @Column(length=5)
 	private int cod_gimnasio;
+	
+	
+	
+	
 	/*GETTERS AND SETTERS*/
 	public int getCod_gimnasio() {
 		return cod_gimnasio;
